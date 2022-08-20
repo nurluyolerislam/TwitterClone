ff88075cc041127e27889ede81a9c5dea70da40b
nurluyolerislam <nurluyolerislam@gmail.com>
2022-08-20 12:13:52 +0300

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Erislam Nurluyol on 20.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
